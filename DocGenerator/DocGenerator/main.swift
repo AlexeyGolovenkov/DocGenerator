fe70bb6f0f9acd6acd05882497a77b386dbd218b
Alexey Golovenkov <mobidevelop@gmail.com>
2016-03-26 19:13:20 +0300

    Start app

---
+++ DocGenerator/DocGenerator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DocGenerator
+//
+//  Created by Alexey Golovenkov on 26.03.16.
+//  Copyright © 2016 Alex & Igor. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
